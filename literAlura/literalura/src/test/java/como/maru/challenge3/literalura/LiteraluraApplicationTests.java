c1252df01dfc10dfb52f08bf21a0ccd0b946cd4c
Maru <jazmin1uwu1@gmail.com>
2024-12-31 16:18:03 -0300

    inicializando repositorio

---
+++ literAlura/literalura/src/test/java/como/maru/challenge3/literalura/LiteraluraApplicationTests.java
@@ -0,0 +1,13 @@
+package como.maru.challenge3.literalura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
